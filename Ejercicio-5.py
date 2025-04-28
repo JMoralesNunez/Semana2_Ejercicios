fb08f9b65839f01af67cb01a0840e9f692a41fb1
JMoralesNunez <136384939+JMoralesNunez@users.noreply.github.com>
2025-04-28 18:10:44 -0500

    else correction

--- Ejercicio-5.py
+++ Ejercicio-5.py
@@ -1,7 +1,7 @@
-num1 = int(input("enter the fisrt number: "))
+num1 = int(input("enter the first number: "))
 num2 = int(input("enter the second number: "))
 
 if num1 and num2 > 0:
     print("Both numbers are positive!")
 else: 
-    print("Both numbers are negative!")
+    print("At least one number is negative!")
