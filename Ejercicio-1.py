2561f29f5eb14dbd348a6ff0fa881cc8364e0b26
JMoralesNunez <136384939+JMoralesNunez@users.noreply.github.com>
2025-04-28 18:02:44 -0500

    Update Ejercicio-1.py

--- Ejercicio-1.py
+++ Ejercicio-1.py
@@ -3,4 +3,7 @@ password = "1234"
 while True:
     guess = input("Please enter your password: ")
     if guess == password:
+        print("Correct password")
         break
+    else:
+        print("wrong password, try again")
